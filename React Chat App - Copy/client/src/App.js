213216ff567443d4c1b26ca345c1dd52879c3e6b
Arsh Bandlish <84235904+arshbandlish@users.noreply.github.com>
2022-06-25 20:09:43 +0530

    Add files via upload

---
+++ React Chat App - Copy/client/src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Create a Group</h3>
+          <input
+            type="text"
+            placeholder="Enter Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter Group Name..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Start Chatting</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
